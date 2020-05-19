a980289d408b84645cd4342f82a4c3a1df89b25f
David Sawyer <desawyer85@gmail.com>
2020-05-19 16:19:16 -0500

    adds unit testing

---
+++ ProjectOneV3Tests/Controllers/CustomerControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcMovie.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvcMovie.Controllers.Tests
+{
+    [TestClass()]
+    public class CustomerControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,109 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+        [Fact]
+        public void Test2()
+        {
+
+        }
+        [Fact]
+        public void Test3()
+        {
+
+        }
+        [Fact]
+        public void Test4()
+        {
+
+        }
+        [Fact]
+        public void Test5()
+        {
+
+        }
+        [Fact]
+        public void Test6()
+        {
+
+        }
+        [Fact]
+        public void Test7()
+        {
+
+        }
+        [Fact]
+        public void Test8()
+        {
+
+        }
+        [Fact]
+        public void Test9()
+        {
+
+        }
+        [Fact]
+        public void Test10()
+        {
+
+        }
+        [Fact]
+        public void Test11()
+        {
+
+        }
+        [Fact]
+        public void Test12()
+        {
+
+        }
+        [Fact]
+        public void Test13()
+        {
+
+        }
+        [Fact]
+        public void Test14()
+        {
+
+        }
+        [Fact]
+        public void Test15()
+        {
+
+        }
+        [Fact]
+        public void Test16()
+        {
+
+        }
+        [Fact]
+        public void Test17()
+        {
+
+        }
+        [Fact]
+        public void Test18()
+        {
+
+        }
+        [Fact]
+        public void Test19()
+        {
+
+        }
+        [Fact]
+        public void Test20()
+        {
+
+        }
+    }
+}
